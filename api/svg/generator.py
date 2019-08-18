1bf0acea2b839acb8b4c270b6f71ab0868cf06bf
Samnan Rahee <namanush.rsr.16@gmail.com>
2019-08-18 16:51:23 +0600

    improve color palette. fix #2

--- api/svg/generator.py
+++ api/svg/generator.py
@@ -11,9 +11,9 @@ class SVG_map:
         self.__progress_details_x__ = self.__get_progress_details_X__(self.__progress__)
 
     def __set_default__(self):
-        self.__green__ = "fill:rgb(100,255,100)"
-        self.__red__ = "fill:rgb(255,100,100)"
-        self.__yellow__ = "fill:rgb(255,250,100)"
+        self.__green__ = "fill:rgb(195,232,141)"
+        self.__red__ = "fill:rgb(240,113,120)"
+        self.__yellow__ = "fill:rgb(255,203,107)"
         self.__keys__ = (
             "start",
             "style",
@@ -25,7 +25,7 @@ class SVG_map:
             "error_text",
             "end",
         )
-    
+
     def __get_progress_details_X__(self, progress):
         if progress <= 9:
             return self.__title_width__ + 25
@@ -51,13 +51,13 @@ class SVG_map:
             + "</style>",
             "title_rect": '<rect rx="3" width="'
             + str(self.__title_width__)
-            + '" height="20" style="fill:rgb(100,100,255)" />',
-            "title": '<text x="5" y="14" class="bold" style="fill:rgb(250,250,250)">'
+            + '" height="20" style="fill:rgb(15,17,26)" />',
+            "title": '<text x="5" y="14" class="bold" style="fill:rgb(199,146,234)">'
             + self.__title__
             + "</text>",
             "progress_box": '<rect x="'
             + str(self.__title_width__ - 5)
-            + '" rx="3" width="70" height="20" style="fill:rgb(200,200,200)" />',
+            + '" rx="3" width="70" height="20" style="fill:rgb(70,75,93)" />',
             "progress_bar": '<rect x="'
             + str(self.__title_width__ - 5)
             + '" rx="3" width="'
@@ -67,7 +67,7 @@ class SVG_map:
             + '" />',
             "progress_details": '<text x="'
             + str(self.__progress_details_x__)
-            + '" y="14" class="txt" style="fill:rgb(5,5,5); align:center">'
+            + '" y="14" class="txt" style="fill:rgb(255,255,255); align:center">'
             + str(self.__progress__)
             + "%</text>",
             "error_text": "Sorry, your browser does not support inline SVG.",
