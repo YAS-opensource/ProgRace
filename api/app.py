66d35298000f6cda0dec508c5b3a04388fe176a0
Samnan Rahee <namanush.rsr.16@gmail.com>
2019-08-23 02:00:39 +0600

    document api/app.py

--- api/app.py
+++ api/app.py
@@ -7,7 +7,24 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def svg():
+    """
+    Arguments
+    ---------
+    title : str
+        title of the progress-bar
+    progress : str
+        progress of work
+    total : str
+        total amount of work
+
+    Returns
+    -------
+
+    svg
+        returns the generated svg
+    """
+
     data = request.args
     TITLE = None
     PROGRESS = None
@@ -33,14 +50,15 @@ def hello():
         svg_generator = SVG_map(title=TITLE, total=TOTAL)
     else:
         svg_generator = SVG_map(title=TITLE, progress=PROGRESS, total=TOTAL)
+
     response = Response(
         svg_generator.generate(), mimetype="image/svg+xml;charset=utf-8"
     )
-    # response.headers['Content-Encoding'] = 'gzip'
 
     return response
 
 
 if __name__ == "__main__":
+    # Use port 3000 if no PORT variable found in environment
     PORT = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=PORT)
