75b077ba62d8ad05ef8b0e492e650888b2b8d172
Sihan Tawsik <sihantawsik@gmail.com>
2020-05-10 17:10:30 +0600

    updated tests

--- manage.py
+++ manage.py
@@ -16,16 +16,6 @@ COV = coverage.coverage(
 COV.start()
 
 @manager.command
-def test():
-    """Runs the unit tests without test coverage."""
-    tests = unittest.TestLoader().discover("src/tests", pattern="test*.py")
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
-
-
-@manager.command
 def cov():
     """Runs the unit tests with coverage."""
     tests = unittest.TestLoader().discover("src/tests")
