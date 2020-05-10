56c82fcee205640450f98ca1ae485761e9f7f059
Sihan Tawsik <sihantawsik@gmail.com>
2020-05-10 17:29:47 +0600

    updated wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,1 @@
-import os
-
-from src import app
-
-if __name__ == "__main__":
-    # Use port 3000 if no PORT variable found in environment
-    PORT = int(os.environ.get("PORT", 3000))
-    app.run(host="0.0.0.0", port=PORT)
+from src import app as application
